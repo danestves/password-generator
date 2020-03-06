85d53915850d8fde21d51134fac4526536b9acdd
Daniel Esteves <danestves@Daniels-iMac.local>
2020-03-06 15:40:45 -0400

    TailwindCSS installation

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const autoprefixer = require('autoprefixer');
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./src/**/*.html'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
 
 module.exports = {
-  plugins: [autoprefixer],
-};
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    require('cssnano')
+  ]
+}
