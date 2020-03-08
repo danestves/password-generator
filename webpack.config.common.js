0bd88588eac3b03a33916023bac0de443d83ec95
Daniel Esteves <danestves@Daniels-iMac.local>
2020-03-07 20:04:11 -0400

    Better options for babel-loader

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -25,8 +25,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader'
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       },
       {
         test: /\.html$/,
